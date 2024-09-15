4b2c8537ad261a6aada61b30e5092ae4ec7a6081
leblancr <rich.leblanc1@proton.me>
2024-09-14 21:04:26 -0400

    test lint error

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,6 +1,8 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
+const foo = 'ff'
+
 export default class FooterComponent extends Component {
   @service('todo-data') todos;
 }
